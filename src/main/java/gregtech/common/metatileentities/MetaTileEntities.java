a86dba4db518accf250456db06ffcdff2272081f
tech22 <37029404+TechLord22@users.noreply.github.com>
2022-01-22 22:10:43 -0500

    new names for HT things

--- src/main/java/gregtech/common/metatileentities/MetaTileEntities.java
+++ src/main/java/gregtech/common/metatileentities/MetaTileEntities.java
@@ -806,6 +806,6 @@ public class MetaTileEntities {
     }
 
     public static boolean getHighTier(String key) {
-        return HIGH_TIER.getOrDefault(key, false);
+        return HIGH_TIER.getOrDefault(key, GTValues.HT);
     }
 }
